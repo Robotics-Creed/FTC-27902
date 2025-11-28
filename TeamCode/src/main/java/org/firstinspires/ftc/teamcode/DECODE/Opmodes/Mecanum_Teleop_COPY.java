6d4255f19b5d3e60e765f5ef83228bd465807aa0
Shar-f <sharaf.hossain24@lae.ac.uk>
2025-11-28 19:31:06 +0000

    removed irrelevant code to DECODE season

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DECODE/Opmodes/Mecanum_Teleop_COPY.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DECODE/Opmodes/Mecanum_Teleop_COPY.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.robot.RobotState;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
@@ -22,57 +23,8 @@ public class Mecanum_Teleop_COPY extends LinearOpMode {
     private DcMotor leftFront = null;
     private DcMotor leftBack = null;
     private DcMotor rightFront= null;
-    private DcMotor rightBack = null;
-    private DcMotor arm = null;
-    private DcMotor wrist = null;
-    private Servo claw = null;
-    private CRServo intake = null;
 
-
-    // Arm and Wrist target positions for each state
-    private static final int ARM_POSITION_INIT = 100;
-    private static final int ARM_POSITION_INTAKE = 250;
-    private static final int ARM_POSITION_WALL_GRAB = 4400;
-    private static final int ARM_POSITION_WALL_UNHOOK = 1200;
-    private static final int ARM_POSITION_HOVER_HIGH = 1700;
-    private static final int ARM_POSITION_CLIP_HIGH = 2300;
-    private static final int ARM_POSITION_LOW_BASKET = 1550;
-
-
-    private static final int WRIST_POSITION_INIT = 0;
-    private static final int WRIST_POSITION_SAMPLE = 240;
-    private static final int WRIST_POSITION_SPEC = 0;
-
-
-
-    // Claw positions
-    private static final double CLAW_OPEN_POSITION = 0.52;
-    private static final double CLAW_CLOSED_POSITION = 0.7;
-
-    // Enum for state machine
-    private enum RobotState {
-        INIT,
-        INTAKE,
-        WALL_GRAB,
-        WALL_UNHOOK,
-        HOVER_HIGH,
-        CLIP_HIGH,
-        LOW_BASKET,
-        MANUAL
-    }
-
-    // Initial state
-    private RobotState currentState = RobotState.INIT;
-
-    // Claw toggle state
-    private boolean clawOpen = true;
-    private boolean lastBump = false;
-    private boolean lastHook = false;
-    private boolean lastGrab = false;
-
-    //target position
-    private int targetArm = 0;
-    private int targetWrist = 0;
+    private DcMotor rightBack= null;
 
     @Override
     public void runOpMode() {
@@ -116,18 +68,12 @@ public class Mecanum_Teleop_COPY extends LinearOpMode {
         rightBack.setDirection(DcMotorSimple.Direction.FORWARD);
         rightFront.setDirection(DcMotorSimple.Direction.FORWARD);
 
-        arm = hardwareMap.get(DcMotor.class, "arm");
-        wrist = hardwareMap.get(DcMotor.class, "wrist");
-        claw = hardwareMap.get(Servo.class, "claw");
-        intake = hardwareMap.get(CRServo.class, "intake");
 
         // Stop and reset encoders
         leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        wrist.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         // Set motors to use encoders
         leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -137,15 +83,10 @@ public class Mecanum_Teleop_COPY extends LinearOpMode {
 
 
         //Set zero power behavior
-        wrist.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        opModeInInit();
-        claw.setPosition(CLAW_CLOSED_POSITION);
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -154,109 +95,11 @@ public class Mecanum_Teleop_COPY extends LinearOpMode {
         while (opModeIsActive()) {
 
             // State machine logic
-            switch (currentState) {
-                case INIT:
-                    targetArm = ARM_POSITION_INIT;
-                    targetWrist = WRIST_POSITION_INIT;
-                    telemetry.addData("State", "INIT");
-                    break;
-                case INTAKE:
-                    targetArm = ARM_POSITION_INTAKE;
-                    targetWrist = WRIST_POSITION_SAMPLE;
-                    telemetry.addData("State", "INTAKE");
-                    break;
 
-                case WALL_GRAB:
-                    targetArm = ARM_POSITION_WALL_GRAB;
-                    targetWrist = WRIST_POSITION_SPEC;
-                    telemetry.addData("State", "WALL_GRAB");
-                    break;
-
-                case WALL_UNHOOK:
-                    targetArm = ARM_POSITION_WALL_UNHOOK;
-                    targetWrist = WRIST_POSITION_SPEC;
-                    telemetry.addData("State", "WALL_UNHOOK");
-                    break;
-
-                case HOVER_HIGH:
-                    targetArm = ARM_POSITION_HOVER_HIGH;
-                    targetWrist = WRIST_POSITION_SPEC;
-                    telemetry.addData("State", "HOVER_HIGH");
-                    break;
-                    
-                case CLIP_HIGH:
-                    targetArm = ARM_POSITION_CLIP_HIGH;
-                    targetWrist = WRIST_POSITION_SPEC;
-                    telemetry.addData("State", "CLIP_HIGH");
-                    break;
-                case LOW_BASKET:
-                    targetArm = ARM_POSITION_LOW_BASKET;
-                    targetWrist = WRIST_POSITION_SAMPLE;
-                    telemetry.addData("State", "LOW_BASKET");
-                    break;
-                case MANUAL:
-                    telemetry.addData("State", "MANUAL");
-                    break;
             }
-            
-            
-            
 
             // Handle state transitions based on gamepad input
-            if (gamepad1.a) {
-                currentState = RobotState.INTAKE;
-            } else if (gamepad1.b && !lastGrab) {
-                if(currentState == RobotState.WALL_GRAB){
-                    currentState = RobotState.WALL_UNHOOK;
-                }else{
-                    currentState = RobotState.WALL_GRAB;
-                }
-            } else if (gamepad1.y && !lastHook) {
-                if(currentState == RobotState.HOVER_HIGH){
-                    currentState = RobotState.CLIP_HIGH;
-                }else{
-                    currentState = RobotState.HOVER_HIGH;
-                }
-            } else if (gamepad1.x) { 
-                currentState = RobotState.LOW_BASKET;           
-            } else if (gamepad1.left_bumper) {
-                currentState = RobotState.INIT;
-            } else if (gamepad1.dpad_up){ //manual control
-                currentState = RobotState.MANUAL;
-                targetArm += 10;
-            } else if (gamepad1.dpad_down){
-                currentState = RobotState.MANUAL;
-                targetArm -= 10;
-            } else if (gamepad1.dpad_left){
-                currentState = RobotState.MANUAL;
-                targetWrist += 1;
-            } else if (gamepad1.dpad_right){
-                currentState = RobotState.MANUAL;
-                targetWrist -= 1;
-            }
-            
-            lastGrab = gamepad1.b;
-            lastHook = gamepad1.y;
 
-            // Toggle claw position when right_bumper is pressed
-            if (gamepad1.right_bumper && !lastBump) {
-                clawOpen = !clawOpen;
-                if (clawOpen) {
-                    claw.setPosition(CLAW_OPEN_POSITION);
-                } else {
-                    claw.setPosition(CLAW_CLOSED_POSITION);
-                }
-            }
-            lastBump = gamepad1.right_bumper;
-
-            // Control intake servo with triggerss
-            if (gamepad1.right_trigger>0.1) {
-                intake.setPower(1.0);
-            } else if (gamepad1.left_trigger>0.1) {
-                intake.setPower(-1.0);
-            } else {
-                intake.setPower(0);
-            }
             
             //DRIVE
             double max = 0.8;
@@ -284,22 +127,8 @@ public class Mecanum_Teleop_COPY extends LinearOpMode {
             leftBack.setPower(leftBackPower);
             rightBack.setPower(rightBackPower);
 
-
-
-            arm.setTargetPosition(targetArm);
-            arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            wrist.setTargetPosition(targetWrist);
-            wrist.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            arm.setPower(1);
-            wrist.setPower(1);
-
             // Send telemetry data to the driver station
             telemetry.addData("Status", "Initialized");
-            telemetry.addData("Claw Position", clawOpen ? "Open" : "Closed");
-            telemetry.addData("Arm Position", arm.getCurrentPosition());
-            telemetry.addData("Arm Power", arm.getPower());
-            telemetry.addData("Wrist Position", wrist.getCurrentPosition());
-            telemetry.addData("Wrist Power", wrist.getPower());
             telemetry.addData("Position", data);
 
 
@@ -307,4 +136,4 @@ public class Mecanum_Teleop_COPY extends LinearOpMode {
         }
 
     }
-}
+
