d495f69f1aa59c6e6b69fc4d257f1d152b31bec8
FifiG69 <zf.gao@hotmail.com>
2026-02-12 15:13:15 +0000

    Update AprilTagLimelightTest.java
    Signed-off-by: FifiG69 <zf.gao@hotmail.com>

--- TeamCode/AprilTagLimelightTest.java
+++ TeamCode/AprilTagLimelightTest.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.hardware.limelightvision.LimeLight3A;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.LLResult;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
@@ -14,7 +15,7 @@ public class AprilTagsLimeLightTest extends OpMode {
   
   @Override
 public void init() {
-  limelight = hardwareMap.get(LimeLight3A.class, deviceName: "LimeLight");
+  limelight = hardwareMap.get(LimeLight3A.class, deviceName: "Limelight");
   limelight.pipelineSwitch(index:8); //april tag #11 pipeline
   imu = hardwareMap.get(IMU.class, deviceName: "imu");
   RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.FORWARD);
@@ -33,10 +34,11 @@ public void loop() {
   limelight.updateRobotOrientation(orientation, getYaw());
   LLResult LLResult = limelight.getLatestResult();
   if (llResult != null && llResult.isValid()) {
-    Pose3D botPose = llResult.getBotpose_MT2();
+    Pose3D botPose = llResult.getBotPose_MT2();
     telemetry.addData(caption: "Tx", llResult.getTx());
     telemetry.addData(caption: "Ty", llResult.getTy());
     telemetry.addData(caption: "Ta", llResult.getTa());
-}
 
+}
+  telemetry.update();
 }
